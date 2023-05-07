4d03c21b23628fd5ed50c041223a87eb7e6aa6fe
zack8361 <zack6353@naver.com>
2023-05-07 17:30:21 +0900

    프로젝트 세팅

---
+++ src/main/java/UsedStore/storeback/StoreBackApplication.java
@@ -0,0 +1,13 @@
+package UsedStore.storeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UsedStore/storeback/StoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package UsedStore.storeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
