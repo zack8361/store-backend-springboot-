a4c160df47bc08d79505c3a87d644c4358f5e7f8
zack8361 <zack6353@naver.com>
2023-07-26 17:25:34 +0900

    도대쳉 ㅗ애안옹ㅁ
    도대쳉 ㅗ애안옹ㅁ

--- src/main/java/UsedStore/Controller/ChatController.java
+++ src/main/java/UsedStore/Controller/ChatController.java
@@ -6,17 +6,17 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-
 @Controller
 public class ChatController {
 
     @MessageMapping("/app/message")
     @SendTo("/topic/chat")
-    public ChatVO handleMessage(@Payload ChatVO chatVO){
-
-        System.out.println("메세지가 도착했습니다");
+    public ChatVO handleMessage(@Payload ChatVO chatVO) {
+        System.out.println("메시지가 도착했습니다");
         System.out.println(chatVO.getUserId());
         System.out.println(chatVO.getMessage());
+
+        // 받은 메시지를 그대로 리턴하거나, 필요에 따라 추가 작업 수행 후 리턴
         return chatVO;
     }
 }
